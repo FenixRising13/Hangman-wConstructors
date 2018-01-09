a9abae500f6011b7e25bd1f5e33279e0fd120604
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-01-08 21:15:09 -0500

    Additional file structure. Setting up constructors for letters and words.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var Inquirer = require("inquirer");
 var Chalk = require("chalk");
 var word = require("./Word");
 var words = require("./words");
+var letters = require("./letters")
 
 console.log(Chalk.green.bold('Our TAs are the best!'));
 
@@ -15,3 +16,36 @@ function Game() {
 }
 
 Game();
+
+var guessesLeft = 10
+
+var askQuestion = function() {
+    // if statement to ensure that we only give 10 guesses
+    if (guessesLeft > 0) {
+      // runs inquirer and asks the user a series of questions whose replies are
+      // stored within the variable answers inside of the .then statement
+      Inquirer.prompt([
+        {
+          name: "letters",
+          message: "Choose a letter?"
+        }
+      ]).then(function(answers) {
+        // initializes the variable Letters to be part of the letters object which will take
+        // in all of the user's answers to the questions above
+        var Letters = new letters(
+          answers.letters);
+        // printInfo method is run to show that the newguy object was successfully created and filled
+        letters.printInfo();
+        // add one to count to increment our recursive loop by one
+        count--;
+        // run the askquestion function again so as to either end the loop or ask the questions again
+        askQuestion();
+      });
+      // else statement which prints "all questions asked" to the console
+      // when the code has been run five times
+    }
+    else {
+      console.log("All guesses used");
+    }
+  };
+  askQuestion();
